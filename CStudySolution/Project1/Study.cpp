ead394d7c35ab3f7e5d8016081662df0b84c1465
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2024-07-28 18:25:58 +0900

    Making my function library

--- CStudySolution/Project1/Study.cpp
+++ CStudySolution/Project1/Study.cpp
@@ -1,55 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#define CHECK_CNT
-#define LEVEL 2
+#include "SortAlgorithm.h"
 
 int main()
 {
-	int *NumberArray;
-	int NumberArrayLength = -1;
-	int sum = 0;
-	float result = -1.f;
-
-	scanf_s("%d", &NumberArrayLength, sizeof(NumberArrayLength));
-
-	NumberArray = (int*)malloc(sizeof(*NumberArray)*NumberArrayLength);
-
-	if (NumberArray == NULL) //assert
-	{
-		printf("Memory allocate fail on NumberArray");
-		return 1;
-	}
-
-	for (int i = 0; i < NumberArrayLength; i++) //input
-	{
-		scanf_s("%d", &NumberArray[i]);
-		if (NumberArray[i] <= 0)
-		{
-			printf("need to input only positive number");
-			return 1;
-		}
-		sum += NumberArray[i];
-
-		#ifdef CHECK_CNT
-			printf("Positive Number count : %d, Sum value : %d\n", i+1, sum);
-		#endif // print count and sum
-		
-		#if LEVEL == 2
-			printf("Current Average Value %f\n", (float)sum/float(i+1));
-		#endif
-	}
-
-	if (NumberArrayLength != 0)
-	{
-		result = sum/NumberArrayLength;
-		printf("total average value : %f", result);
-	}
-	else if (NumberArrayLength == -1)
-	{
-		printf("NumberArrayLength is -1, it's not initialize");
-		return 1;
-	}
+	int IntArray[] = {2,6,3,7,5,4,7,7,4};
+	
+	SelectSort(IntArray, 9, true);
+	PrintIntArray(IntArray, 9);
 
 	return 0;
 }
