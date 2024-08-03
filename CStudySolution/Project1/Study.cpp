dcca3e18a33a3f4b0f3e0caf2fa8070da7b15388
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2024-08-04 00:13:44 +0900

    Created Employee example code on study.h

--- CStudySolution/Project1/Study.cpp
+++ CStudySolution/Project1/Study.cpp
@@ -1,27 +1,2 @@
 #include "Study.h"
 
-Point PointCnt::PointAdder(const Point& pnt1, const Point& pnt2)
-{
-    cnt++;
-    return Point(pnt1.x+pnt2.x, pnt1.y+pnt2.y);
-}
-
-Point PointCnt::PointSub(const Point& pnt1, const Point& pnt2)
-{
-    cnt++;
-    return Point(pnt1.x-pnt2.x, pnt1.y-pnt2.y);   
-}
-
-int main()
-{
-    Point a(1,2);
-    Point b(3,4);
-    PointCnt ct;
-
-    ShowPointPos(ct.PointAdder(a,b));
-    ShowPointPos(ct.PointAdder(b,a));
-
-    return 0;
-
-    
-}
--- CStudySolution/Project1/Study.h
+++ CStudySolution/Project1/Study.h
@@ -1,35 +1,145 @@
 ﻿#pragma once
+
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
-class Point;
-class PointCnt
+class Employee
 {
 private:
-    int cnt = 0;
+    char name[100];
 public:
-    ~PointCnt()
+    Employee(const char*name)
     {
-        cout<<"calculate count is "<<cnt<<endl;
+        strcpy_s(this->name, strlen(name)+1, name);
     }
-    Point PointAdder(const Point&, const Point&);
-    Point PointSub(const Point&, const Point&);
+    void ShowYourName() const
+    {
+        cout<<"name : "<<name<<endl;
+    }
+
+    virtual int GetPay() const = 0;
+    virtual void ShowSalaryInfo() const = 0;
 };
 
-class Point
+class TemporaryWorker : public Employee
 {
 private:
-    int x,y;
-
+    int workTime;
+    int payPerHour;
 public:
-    Point(const int& xpos, const int& ypos):x(xpos), y(ypos){}
-   
-    friend Point PointCnt::PointAdder(const Point&, const Point&);
-    friend Point PointCnt::PointSub(const Point&, const Point&);
-    friend void ShowPointPos(const Point&);
+    TemporaryWorker(const char * name, int pay):Employee(name), workTime(0), payPerHour(pay){}
+    void AddWorkTime(int time)
+    {
+        workTime+=time;
+    }
+    virtual int GetPay() const
+    {
+        return workTime*payPerHour;
+    }
+    virtual void ShowSalaryInfo() const
+    {
+        ShowYourName();
+        cout<<"Salary : "<<GetPay()<<endl;
+    }
 };
 
-void ShowPointPos(const Point &pos)
+class PermanentWorker : public Employee
 {
-    cout<<"x : "<<pos.x<<"y : "<<pos.y<<endl;
+private:
+    int salary;
+public:
+    PermanentWorker(const char* name, int money):Employee(name), salary(money){}
+    virtual int GetPay() const
+    {
+        return salary;
+    }
+    virtual void ShowSalaryInfo() const
+    {
+        ShowYourName();
+        cout<<"Salary : "<<salary<<endl;
+    }
+};
+
+class SalesWorker : public PermanentWorker
+{
+private:
+    int salesResult;
+    double bonusRatio;
+public:
+    SalesWorker(const char * name, int money, double ratio)
+    : PermanentWorker(name, money), salesResult(0), bonusRatio(ratio){}
+
+    void AddSalesResult(int value)
+    {
+        salesResult+=value;
+    }
+    virtual int GetPay() const
+    {
+        return PermanentWorker::GetPay() + (int)(salesResult*bonusRatio);
+    }
+
+    virtual void ShowSalaryInfo() const
+    {
+        ShowYourName();
+        cout<<"Salary : "<<GetPay()<<endl;
+    }
+};
+class EmployeeHandler
+{
+private:
+    Employee* empList[50];
+    int empNum;
+public:
+    EmployeeHandler() : empNum(0){}
+    void AddEmployee(Employee* emp)
+    {
+        empList[empNum++] = emp;
+    }
+    void ShowAllSalaryInfo() const
+    {
+        for(int i = 0; i<empNum; i++)
+        {
+            empList[i]->ShowSalaryInfo();
+        }
+    }
+    void ShowTotalSalary() const
+    {
+        int sum = 0;
+        for (int i = 0; i <empNum; i++)
+        {
+            sum += empList[i]->GetPay();
+        }
+        cout <<"Total Salary : "<<sum<<endl;
+    }
+    ~EmployeeHandler()
+    {
+        for (int i = 0; i<empNum; i++)
+        {
+            delete empList[i];
+        }
+    }
+};
+
+int main()
+{
+    EmployeeHandler handler;
+
+    handler.AddEmployee(new PermanentWorker("KIM", 1000));
+    handler.AddEmployee(new PermanentWorker("LEE", 1500));
+    handler.AddEmployee(new PermanentWorker("JUN", 2000));
+
+    TemporaryWorker * alba = new TemporaryWorker("JUNG", 700);
+    alba->AddWorkTime(5);
+    handler.AddEmployee(alba);
+
+    SalesWorker * seller=new SalesWorker("Hong", 1000, 0.1);
+    seller->AddSalesResult(7000);
+    handler.AddEmployee(seller);
+    
+    handler.ShowAllSalaryInfo();
+    handler.ShowTotalSalary();
+
+    return 0;
 }
