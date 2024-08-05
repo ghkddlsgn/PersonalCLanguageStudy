5e818953d8cd606064a5b7822e6f0fc0eb6be59a
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2024-08-05 19:04:25 +0900

    operator override complete p445

--- CStudySolution/Project1/Study.h
+++ CStudySolution/Project1/Study.h
@@ -38,6 +38,37 @@ public:
 
         return *this;
     }
+    Point& operator++()
+    {
+        x++;
+        y++;
+        return *this;
+    }
+    Point operator++(int)
+    {
+        const Point ReturnValue(x,y);
+        x++;
+        y++;
+        return ReturnValue;
+    }
+    Point& operator--()
+    {
+        x--;
+        y--;
+        return *this;
+    }
+    Point operator*(int a) const
+    {
+        const Point ReturnValue(x*a, y*a);
+        return ReturnValue;
+    }
+    const Point operator--(int)
+    {
+        const Point ReturnValue(*this);
+        x--;
+        y--;
+        return ReturnValue;
+    }
     bool operator==(const Point &ref) const
     {
         if (x == ref.x && y == ref.y)
@@ -57,30 +88,37 @@ public:
     }
 
     friend Point operator~(const Point &ref);
-    
+    friend ostream& operator<<(ostream& os, const Point &pos);
+    friend istream& operator>>(istream& is, Point &pos);
 };
 
 Point operator~(const Point &ref)
 {
     return {~ref.x, ~ref.y};
 }
+Point operator*(int a, const Point &ref)
+{
+    Point returnValue(ref);
+    return ref.operator*(a);
+}
+ostream& operator<<(ostream& os, const Point &pos)
+{
+    os<<"["<<pos.x<<" "<<pos.y<<"]";
+    return os;
+}
+istream& operator>>(istream& is, Point &pos)
+{
+    is>>pos.x>>pos.y;
+
+    return is;
+}
 
 int main()
 {
-    Point a(10,20);
-    Point b(30,40);
-    Point c;
+    Point pt_A(10,20);
 
-    c+=b;
-
-    a.ShowPosition();
-    b.ShowPosition();
-    c.ShowPosition();
-
-    cout<<"Begin sub a-b"<<endl;
-
-    c = a-b;
-    c.ShowPosition();
-
+    cin>>pt_A;
+    cout<<pt_A;
+    
     return 0;
 }
