c26f702a8a9462879bd4f94b7b2fb19735ae65a7
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2024-08-21 20:05:18 +0900

    Solved Q1342

--- CStudySolution/Project1/Study.h
+++ CStudySolution/Project1/Study.h
@@ -10,53 +10,18 @@ using namespace  std;
 
 class Solution {
 public:
-    vector<string> fizzBuzz(int n)
-    {
-        vector<string> ReturnValue;
-        ReturnValue.resize(n);
-
-        int Fizz = 1;
-        int Buzz = 1;
-        
-        for (int i = 0; i < n; i++)
-        {
-            if (Fizz == 3)
-            {
-                Fizz = 0;
-                if (Buzz == 5)
-                {
-                    Buzz = 0;
-                    ReturnValue[i] = "FizzBuzz";
-                }
-                else
-                {
-                    ReturnValue[i] = "Fizz";
-                }
-            }
-            else if(Buzz == 5)
-            {
-                Buzz = 0;
-                ReturnValue[i] = "Buzz";
-            }
-            else
-            {
-                ReturnValue[i] = to_string(i+1);
-            }
-            Fizz++;
-            Buzz++;
-        }
-        return ReturnValue;
+    int numberOfSteps(int num) {
+        return (num & ~0);
     }
 };
 
 int main()
 {
     Solution AAA;
-    int n = 15;
-
-    vector<string> StringVector = AAA.fizzBuzz(n);
-
-    PrintElements(StringVector);
+    
+    cout<<AAA.numberOfSteps(14)<<endl;;
+    cout<<AAA.numberOfSteps(8)<<endl;
+    cout<<AAA.numberOfSteps(123)<<endl;
     
     return 0;
 }
