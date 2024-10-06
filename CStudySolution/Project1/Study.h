ccb9b095439d23073f858a1b83204519a7fec70d
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2024-10-06 13:46:44 +0200

    Create Queue Class

--- CStudySolution/Project1/Study.h
+++ CStudySolution/Project1/Study.h
@@ -1,15 +1,17 @@
 #pragma once
 
 #include <iostream>
-
-#include "DataStructure/List/InhuCircularList.h"
+#include "DataStructure/Queue/InhuCircularQueue.h"
 
 using namespace std;
 
 int main()
 {
-    InhuCircularList A(10,20,30,40,50,60);
-    A.Insert(0,0);
-    A.PrintList();
+    InhuCircularQueue A = {10,10,20,30,40,50};
+    A.Print();
+    A.AddNewElement(123456);
+    A.Print();
+    A.DeleteElement();
+    A.Print();
     return 0;
 }
