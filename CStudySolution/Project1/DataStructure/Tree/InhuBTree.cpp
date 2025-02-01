3bf65997ca2ce1c79fdea6d1559e57e2dee050be
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2025-02-01 16:02:25 +0100

    updating functions

--- CStudySolution/Project1/DataStructure/Tree/InhuBTree.cpp
+++ CStudySolution/Project1/DataStructure/Tree/InhuBTree.cpp
@@ -1,5 +1,15 @@
+#pragma once
 #include <iostream>
 #include "InhuBTree.h"
+#include "Project1/SortAlgorithm.h"
+
+TreeN_Node* InhuBTree::CreateNewNode()
+{
+    TreeN_Node* newNode = new TreeN_Node(node_size);
+    NodeArr[size] = newNode;
+    size++;
+    return newNode;
+}
 
 void InhuBTree::DeleteAllNodesFromBelow(TreeN_Node* node)
 {
@@ -12,10 +22,125 @@ void InhuBTree::DeleteAllNodesFromBelow(TreeN_Node* node)
     size--;
 }
 
-void InhuBTree::SortNodeValue(TreeN_Node* node)
+/*
+if NewChildNode is nullptr, it means i split leaf node
+if NewChildNode is not nullptr, it means i split internal node.
+i need to put NewChildNode after Insert point
+*/
+void InhuBTree::SplitNode(TreeN_Node* node, int NewValue, TreeN_Node* NewChildNode)
 {
+    //check node is full
+    if (node->len != node_size)
+    {
+        std::cout << "Node is not full" << std::endl;
+        return;
+    }
+
+    int i = 0;
+
+    //prepare parent node
+    TreeN_Node* parentNode = node->ParentNode;
+    if (parentNode == nullptr)
+    {
+        parentNode = CreateNewNode();
+        parentNode->ChildNodeArr[0] = node;
+        node->ParentNode = parentNode;
+    }
+    //create new nodes for split
+    TreeN_Node* newNode = CreateNewNode();
+    newNode->ParentNode = parentNode;
+
+
+    int insert_index = GetInsertTargetIndex(NewValue, node);
+
+    //create whole node values and child nodes
+    int new_whole_node_values[node_size + 1];
+    TreeN_Node* new_whole_node_child_nodes[node_size + 2];
+
+    for (i = 0; i <insert_index - 1; i++)
+    {
+        new_whole_node_values[i] = node->Value[i];
+        new_whole_node_child_nodes[i] = node->ChildNodeArr[i];
+    }
+    new_whole_node_child_nodes[insert_index] = node->ChildNodeArr[insert_index];
+    new_whole_node_values[insert_index] = NewValue;
+    new_whole_node_child_nodes[insert_index + 1] = NewChildNode; //this could be nullptr
+
+    for (i = insert_index + 1; i < node_size + 1; i++)
+    {
+        new_whole_node_values[i] = node->Value[i];
+        new_whole_node_child_nodes[i+1] = node->ChildNodeArr[i];
+    }
+
+    //initiate split node
+    int split_index = (node_size/2) - 1;
     
+    for (i = 0; i < split_index; i++)
+    {
+        node->Value[i] = new_whole_node_values[i];
+        node->ChildNodeArr[i] = new_whole_node_child_nodes[i];
+    }
+    for (i = split_index; i < node_size; i++)
+    {
+        node->Value[i] = NULLVALUE;
+        
+    }
+
+    //add 1 value to parent node
+
+    int j = 0;
+    for (i = split_index + 1; i < node_size + 1; i++)
+    {
+        newNode->Value[j] = new_whole_node_values[i];
+        newNode->ChildNodeArr[j] = new_whole_node_child_nodes[i];
+        j++;
+    }
 }
+
+int InhuBTree::GetInsertTargetIndex(int TargetValue, TreeN_Node* node) const
+{
+    int len = node->len;
+    
+    int insert_index =  len-1/2;
+    int min_index = 0;
+    int max_index = len-1;
+
+
+    while(min_index <= max_index)
+    {
+        if (TargetValue == node->Value[insert_index])
+        {
+            return insert_index;
+        }
+        else if (TargetValue < node->Value[insert_index])
+        {
+            max_index = insert_index - 1;
+        }   
+        else if (TargetValue > node->Value[insert_index])
+        {
+            min_index = insert_index + 1;
+        }
+        insert_index = (min_index + max_index) / 2;
+    }
+    
+    if (min_index != max_index) //logic error check
+    {
+        std::cout << "GetInsertTargetIndex error : min_index != max_index" << std::endl;
+        return -1;
+    }
+
+    /*from now, min_index == max_index, so i need to figure out 
+    whether insert before or after insert_index*/
+    if (TargetValue < node->Value[insert_index])
+    {
+        return insert_index; //insert before
+    }
+    else
+    {
+        return insert_index + 1; //insert after
+    }
+}
+
 //it return last searched node, so if i want to add new node, i can add at last searched node
 bool InhuBTree::SearchNode(int TargetValue, TreeN_Node *& LastSearchedNode) const
 {
@@ -64,6 +189,7 @@ bool InhuBTree::SearchNode(int TargetValue, TreeN_Node *& LastSearchedNode) cons
     }
 }
 
+//it always add new value on leaf node or split node 
 bool InhuBTree::AddNewValue(int NewValue)
 {
     TreeN_Node* TargetNode;
@@ -77,26 +203,15 @@ bool InhuBTree::AddNewValue(int NewValue)
     if (TargetNode->len == node_size)
     {
         //start split node
-        
-        //sort value
-        int temp[TargetNode->len + 1];
-        for (int i = 0; i < TargetNode->len; i++)
-        {
-            temp[i] = TargetNode->Value[i];
-        }
-        //add new value to temp array
-        temp[TargetNode->len] = NewValue;
-        //sort temp array
-        SortNodeValue(TargetNode);
+        SplitNode(TargetNode, NewValue);
 
     }
     else
     {
-        //add new value to target node's empty slot
+        //add new value to target node's empty slot -> this will always happen on leaf node
         TargetNode->Value[TargetNode->len] = NewValue;
         TargetNode->len++;
-        SortNodeValue(TargetNode);
-        return true;
+        SelectSort(TargetNode->Value, TargetNode->len, true);
     }
 }
 
--- CStudySolution/Project1/DataStructure/Tree/InhuBTree.h
+++ CStudySolution/Project1/DataStructure/Tree/InhuBTree.h
@@ -1,13 +1,18 @@
+#define NULLVALUE -9999999
 struct TreeN_Node {
     int* Value;
-    TreeN_Node** ChildNodeArr;
-    TreeN_Node* ParentNode;
+    TreeN_Node** ChildNodeArr = nullptr;
+    TreeN_Node* ParentNode = nullptr;
     int Height = 0;
-    int len = 0;
+    int len = 0; //current node value count
     TreeN_Node(int NodeSize)
     {
         Value = new int[NodeSize];
         ChildNodeArr = new TreeN_Node*[NodeSize + 1];
+        for (int i = 0; i < NodeSize; i++) //init value as -9999999
+        {
+            Value[i] = NULLVALUE;
+        }
     }
     ~TreeN_Node()
     {
@@ -17,14 +22,21 @@ struct TreeN_Node {
 };
 
 class InhuBTree {
+
+private:
     TreeN_Node* RootNode = nullptr;
+    TreeN_Node** NodeArr = nullptr;
     int node_size = 0; //single node value length
     int size = 0; //total node count
 protected:
+    TreeN_Node* CreateNewNode();
     void DeleteAllNodesFromBelow(TreeN_Node* node);
-    void SplitNode(TreeN_Node* node, int NewValue);
+    void AddNewValueOnNode(TreeN_Node* node, int NewValue);
+    void SplitNode(TreeN_Node* node, int NewValue, TreeN_Node* NewChildNode=nullptr);
+    int GetInsertTargetIndex(int TargetValue, TreeN_Node* node) const;
 public:
     template <typename... Args>
+
     InhuBTree(int NodeSize,Args... Nodes)
     {
         node_size = NodeSize;
@@ -32,7 +44,16 @@ public:
     }
     ~InhuBTree()
     {
-        DeleteAllNodesFromBelow(RootNode);
+        /*
+        i don't use DeleteAllNodesFromBelow.
+        because i want to ensure all nodes are deleted
+        even some nodes are not properly connected with each other
+        */
+        for (int i = 0; i < size; i++)
+        {
+            delete NodeArr[i];
+        }
+        delete[] NodeArr;
     }
     bool SearchNode(int TargetValue, TreeN_Node *& LastSearchedNode) const;
     bool AddNewValue(int NewValue);
