f4a312d6d9694b15a06316ccd5d7c38a5453259d
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2025-04-05 17:33:22 +0200

    Update UniqueBinarySearchTreesII.py

--- CStudySolution/Project1/LeetCode/UniqueBinarySearchTreesII.py
+++ CStudySolution/Project1/LeetCode/UniqueBinarySearchTreesII.py
@@ -10,27 +10,29 @@ class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
         if n == 0:
             return []
+
+        # memoization = [ [] for _ in range(0, n+1)]
             
-        Memory = [[] for _ in range(n+1)]
-        Memory[0].append(None)
-        Memory[1].append(TreeNode(1))
+        def recursive(start : int, end : int) -> List[Optional[TreeNode]]:
 
-        def update(CurrentN : int):
+            if start > end :
+                return [None]
 
-            for left_child_index in range(0,n+1):
-                new_head = TreeNode(n)
-                
-                left_total_variation = len(Memory[left_child_index])
-                right_total_variation = len(Memory[n-left_child_index])
+            all_trees = []
 
-                for current_left_variation in range(0, left_total_variation):
-                    for current_right_variation in range(0, right_total_variation):
-                        new_head.left = Memory[left_child_index][current_left_variation]
-                        new_head.right = Memory[n-left_child_index][current_right_variation]
-                        Memory[n].append(new_head)
+            for root_val in range(start, end+1):
+                #left
+                left_subtrees = recursive(start, root_val - 1)
+                #right
+                right_subtrees = recursive(root_val+1, end)
+
+                for left_subtree in left_subtrees:
+                    for right_subtree in right_subtrees:
+                        new_tree_node = TreeNode(root_val)
+                        new_tree_node.left = left_subtree
+                        new_tree_node.right = right_subtree
+                        all_trees.append(new_tree_node)
             
-            return
+            return all_trees
         
-        update(n)
-
-        return Memory[n]
+        return recursive(1,n)
