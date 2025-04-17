0105a2f2e151c8ba3e86c60fffa57c3ba8c24522
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2025-04-17 20:35:24 +0200

    leetcode
    better performance

--- CStudySolution/Project1/LeetCode/LinkedList/CopyListwithRandomPointer.py
+++ CStudySolution/Project1/LeetCode/LinkedList/CopyListwithRandomPointer.py
@@ -9,37 +9,41 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        originalList = []
-        originalListPtrIndex = []
-        copiedList = []
+        if head == None:
+            return None
 
-        #save original list
+        #dict {randomNode : (index)} <- working as hash table
+        originalNodeDict = {}
+        originalRandomKey = []
+        copiedNodeList = []
+
+        #fill the originalNodeMap
         currentNode = head
+        currentIndex = 0
         while(currentNode):
-            originalList.append(currentNode)
-            originalListPtrIndex.append(-1)
-            currentNode = currentNode.next
-        
-        #save random's index
-        currentNode = head
-        for i in range(0, len(originalList)):
-            if originalList[i].random:
-                originalListPtrIndex[i] = originalList.index(originalList[i].random)
-        
-        #begin copy
-        copiedList = [Node(-1) for _ in range(len(originalList))]
-        for i in range(0, len(originalList)-1):
-            element = copiedList[i]
-            element.val = originalList[i].val
-            element.next = copiedList[i+1]
-            if originalListPtrIndex != -1:
-                element.random = copiedList[originalListPtrIndex[i]]
-        
-        #handle edge(end) case
-        element = copiedList[-1]
-        element.val = originalList[-1].val
-        element.next = None
-        if originalListPtrIndex != -1:
-            element.random = copiedList[originalListPtrIndex[-1]]
+            originalNodeDict[currentNode] = currentIndex
 
-        return copiedList[0]
+            #make new list
+            copiedNodeList.append(Node(currentNode.val, None))
+            #prepare for next iteration
+            currentNode = currentNode.next
+            currentIndex += 1
+        
+        #fill the originalRandomKey
+        for element in originalNodeDict:
+            if element.random:
+                originalRandomKey.append(originalNodeDict[element.random])
+            else:
+                originalRandomKey.append(-1)
+        
+        #link each node
+        for i in range(len(copiedNodeList)-1):
+            copiedNodeList[i].next = copiedNodeList[i+1]
+        
+        #link random
+        for i in range(len(copiedNodeList)):
+            randomIndex = originalRandomKey[i]
+            if randomIndex != -1:
+                copiedNodeList[i].random = copiedNodeList[randomIndex]
+
+        return copiedNodeList[0]
