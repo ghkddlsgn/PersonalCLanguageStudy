5514b1488d7136aa140c2a9db12e6510b49bbca3
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2025-04-23 18:44:02 +0200

    leetcode solved Rotation List

--- CStudySolution/Project1/LeetCode/LinkedList/RotateList.py
+++ CStudySolution/Project1/LeetCode/LinkedList/RotateList.py
@@ -7,57 +7,29 @@ class ListNode:
 
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        def reverseList(head: ListNode, attachTo: ListNode):
-            Dummy = ListNode(-1)
-            curNode = head
-            nextNode = None
-            newHead = Dummy
+        if k == 0 or not head: return head
 
-            while(curNode):
-                nextNode = curNode.next
-                curNode.next = 
-            
-            head.next = attachTo
+        length = 0
 
-        if not head or k <= 0: return head
-
-        ListLength = 0
-        currentNode = head
-
-        chunkA_Length = 0
-        chunkA_head = head
-        chunkA_tail = head
-        chunkB_Length = k
-        chunkB_head = head
-        chunkB_Tail = head
-        #i can sure chunkB_length is more then 0
-
-        #get ListLength, chunkB_Tail
-        while(currentNode):
-            ListLength += 1
-            chunkB_Tail = currentNode
-            if currentNode.next:
-                currentNode = currentNode.next
+        cur = head
+        while(cur):
+            length += 1
+            chunkB_Tail = cur
+            cur = cur.next
         
-        #get chunkA_Length
-        k = k % ListLength
-        chunkA_Length = ListLength - k
-
-        #get chunkA_tail, chunkB_head
-        currentNode = head
-        for _ in range(0, chunkA_Length - 1):
-            currentNode = currentNode.next
-        chunkA_tail = currentNode
-        chunkB_Tail = currentNode.next
-        newHead = chunkA_head
-
-        #check there's no chunkA
-        if chunkA_Length == 0:
-            chunkA_head == None
-            chunkA_tail == None
-            newHead = head
+        k = k % length
+        if k == 0: return head
         
-        #reverse chunkB
-        for _ in range(chunkB_Length):
-            
+
+
+        cur = head
+        for _ in range(length-k-1):
+            cur = cur.next
         
+        chunkA_Tail = cur
+        chunkB_Head = cur.next
+
+        chunkA_Tail.next = None
+        chunkB_Tail.next = head
+
+        return chunkB_Head
