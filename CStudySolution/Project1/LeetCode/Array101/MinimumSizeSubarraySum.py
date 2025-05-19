4bb5e7ba2fabf10bba87cf8133cbe16369d9937e
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2025-05-19 20:45:47 +0200

    Update MinimumSizeSubarraySum.py

--- CStudySolution/Project1/LeetCode/Array101/MinimumSizeSubarraySum.py
+++ CStudySolution/Project1/LeetCode/Array101/MinimumSizeSubarraySum.py
@@ -2,31 +2,29 @@ from typing import List
 
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        shortest = []
-        sum = 0
-        for elem in nums:
-            shortest.append(elem)
-            sum += elem
-
-            if sum > target:
-                closestIdx = [idx for idx in range(len(nums))]
-                while len(closestIdx) > 1: #what if there's many satisfied list?
-                    nextClosestIdx = []
-                    minDist = 1000000000
-                    for idx in closestIdx:
-                        curdist = sum - shortest[idx]
-                        
-                        if curdist == minDist:
-                            nextClosestIdx.append(idx)
-                        elif curdist < minDist:
-                            minDist = curdist
-                            nextClosestIdx = [idx]
-
+        bestWindowLength = 1000000000
+        bestTotalSum = 1000000000
         
-        if sum < target:return 0
+        curWindowIdx = 0
+        curWindowLength = 0
+        curTotalSum = 0
+
+        for i in range(0, len(nums)):
+            curTotalSum += nums[i]
+            curWindowLength += 1
+
+            if curTotalSum < target:
+                continue
+            
+            #shrink window to right as much as possible
+            while(curTotalSum - nums[curWindowIdx] >= target):
+                curTotalSum -= nums[curWindowIdx]
+                curWindowLength -= 1
+                curWindowIdx += 1
+            
+            if (curWindowLength < bestWindowLength):
+                bestWindowLength = curWindowLength
+                bestTotalSum = curTotalSum
         
-        return len(shortest)
-
-a = Solution()
-
-print(a.minSubArrayLen(7, [2,3,1,2,4,3]))
+        if bestWindowLength == 1000000000: return 0
+        return bestWindowLength
