844e94e486dcb5734823a27c7de30d0d61b41c23
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2025-06-09 22:04:26 +0200

    python log study

---
+++ CStudySolution/Project1/pythonBasic/Logging/logExample.py
@@ -0,0 +1,18 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.FileHandler("app1.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger=logging.getLogger("ArithmethicApp")
+
+def add(a,b):
+    result = a + b
+    logger.debug(f"Adding {a} + {b}", result)
+    return result
