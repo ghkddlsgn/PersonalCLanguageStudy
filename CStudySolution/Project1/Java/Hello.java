a0d730eac16a80db9ef0952ae819e73103758c42
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2025-06-10 17:43:00 +0200

    java study start

---
+++ CStudySolution/Project1/Java/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 12;
+        int b = 6;
+        int maxInt = 2_147_483_647;
+        System.out.println("a = " + a);
+        long biglongValue = 2_147_483_647_123L;
+        System.out.println(biglongValue);
+
+        System.exit(0);
+    }
+}
