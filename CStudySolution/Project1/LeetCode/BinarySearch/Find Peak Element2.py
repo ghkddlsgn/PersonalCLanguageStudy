bd6379db7153e3fc2b93ba61f8aaa999d56866c1
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2026-02-28 13:05:53 +0100

    solved peakElement

--- CStudySolution/Project1/LeetCode/BinarySearch/Find Peak Element2.py
+++ CStudySolution/Project1/LeetCode/BinarySearch/Find Peak Element2.py
@@ -1,42 +1,27 @@
 class Solution:
     def findPeakElement(self, nums: list[int]) -> int:
-        def isPeak(i:int) -> bool:
-            left_check = nums[i - 1] < nums[i] if i - 1 >= 0 else True
-            right_check = nums[i] > nums[i + 1] if i + 1 >= len(nums) - 1 else True
-            return (left_check and right_check)
-
-        def IsOtherChunk(l:int, r:int, shouldAcs:bool) -> bool:
-            return nums[l] > nums[r] if shouldAcs else nums[l] < nums[r]
+        if len(nums) == 1:return 0
         
+        #from here, len >= 2
         l,r = 0, len(nums) - 1
-        searchStart:int = 0
-        searchEnd:int = len(nums) - 1
-        ShouldACS = nums[l] < nums[r]
-        step = (searchEnd - searchStart + 1) // 2
 
-        while(step >= 1):
-            if isPeak(l): return l
-            if isPeak(r): return r
-                
-            chunk_num = 2
-            step = (searchEnd - searchStart + 1) // 2
-            for l in range(searchStart, searchEnd + 1, step):
-                r = min(len(nums)-1, l + step)
-                if IsOtherChunk(l, r, ShouldACS):
-                    #exclude chunk
-                    if searchStart == 0:
-                        searchStart = searchStart + step + 1
-                    else:
-                        searchEnd = searchEnd - 1
-                    chunk_num = 2
-                    break
-            else:
-                chunk_num *= 2
+        if nums[l] > nums[l+1]: return l
+        if nums[r-1] < nums[r]: return r
         
-        return searchStart
+        while(l <= r):
+            m = (l + r) // 2
+            if nums[m] < nums[m+1]: #acs?
+                l = m
+            else: #desc?
+                if nums[m-1] < nums[m]: return m #peak?
+                r = m
+        
+        return -12345
+                
+
+
 
 sol = Solution()
-nums = [1, 2, 3, 1]
+nums = [1,2,1,3,5,6,4]
 print("Input:", nums)
-print("Peak element:", sol.findPeakElement(nums))
-
+print("Peak idx:", sol.findPeakElement(nums))
