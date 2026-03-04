32df4cc2f77ea328406ad93d3ecccf1dc7c429e3
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2026-03-04 10:31:12 +0100

    leetcode study

--- CStudySolution/Project1/LeetCode/BinarySearch/Find Smallest Letter Greater Than Target.py
+++ CStudySolution/Project1/LeetCode/BinarySearch/Find Smallest Letter Greater Than Target.py
@@ -1,20 +1,43 @@
 class Solution:
     def nextGreatestLetter(self, letters: list[str], target: str) -> str:
-        #edge case
-        if target <= letters[0] or target >= letters[-1]: return letters[0]
+        if letters[0] > target or letters[-1] <= target:
+            return letters[0]
         
-        l,r = 0, len(letters)
-        while letters[l] <= target:
-            m = (l + r)//2
+        l,r = 0, len(letters) - 1
+        while(l < r):
+            m = (l + r) // 2
             if letters[m] <= target:
                 l = m + 1
             else:
                 r = m
-
+        
         return letters[l]
+
+
 
 print(Solution().nextGreatestLetter(["c","f","j"], "c"))  # Output: f
 print(Solution().nextGreatestLetter(["c","f","j"], "f"))  # Output: j
 print(Solution().nextGreatestLetter(["c","f","j"], "j"))  # Output: c
 print(Solution().nextGreatestLetter(["c","f","j"], "a"))  # Output: c
 print(Solution().nextGreatestLetter(["c","f","j"], "k"))  # Output: c
+
+
+
+
+
+# target 
+# 111223344455
+# l    m     r
+# l m r
+#    lr
+#    m
+#   rl
+
+# 00115577
+
+# target 1
+# 6666777889
+# l        r
+
+# 111111111111111
+
--- CStudySolution/Project1/LeetCode/BinarySearch/Pow(x,n).py
+++ CStudySolution/Project1/LeetCode/BinarySearch/Pow(x,n).py
@@ -2,3 +2,6 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         if x == 0: return 0
         if n == 0: return 1
+        if n < 0:
+            x = (1/x)
+            n = abs(n)
